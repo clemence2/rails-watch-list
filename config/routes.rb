32a5dba7749b7718c9a949d5c4d16a3aac8018f0
clemence ogeron <ogeron.clemence@orange.fr>
2021-11-19 11:38:48 +0100

    Setup Clever Cloud config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :movies, only: [] do
-    resources :bookmarks, only: []
+  resources :lists, only: [:index, :show, :new, :create] do
+    # resources :bookmarks
   end
+
 end
